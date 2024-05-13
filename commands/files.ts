ccf63fe947d9a4621a75556d78abd81e6ef35443
Dorian Latouche <dorian@latouche.dev>
2024-05-13 17:27:14 +0200

    (dev): Improve commande files

--- commands/files.ts
+++ commands/files.ts
@@ -19,8 +19,16 @@ export default class Files extends BaseCommand {
   }
 
   async run() {
+    // This is a command for creating files according to the project convention, it's a basic command that can be greatly improved.
+    // ex: node ace make:files roles dm
+    // need to create migration and model file
+
+    // different name options (ex: for model we need singular name)
     const name = this.data[0]
-    const nameUpper = name.charAt(0).toUpperCase() + name.slice(1)
+    const singularName = name.charAt(name.length - 1) === 's' ? name.slice(0, -1) : name
+    const singularNameUpper = singularName.charAt(0).toUpperCase() + singularName.slice(1)
+    const pluralName = name.charAt(name.length - 1) === 's' ? name : `${name}s`
+    const pluralNameUpper = pluralName.charAt(0).toUpperCase() + pluralName.slice(1)
 
     let options = this.data[1]
     if (options === 'all' || !options) options = 'mcsvd'
@@ -29,45 +37,48 @@ export default class Files extends BaseCommand {
       const codemods = await this.createCodemods()
       const type = 'model'
       await codemods.makeUsingStub(stubsRoot, `../commands/stubs/${type}.stub`, {
-        name: `${name}`,
-        nameUpper,
-        path: this.app.makePath(`app/${type}s/`, `${name}.ts`),
+        name: `${singularName}`,
+        nameUpper: singularNameUpper,
+        path: this.app.makePath(`app/${type}s/`, `${singularName}.ts`),
       })
     }
     if (options.includes('s')) {
       const codemods = await this.createCodemods()
       const type = 'service'
       await codemods.makeUsingStub(stubsRoot, `../commands/stubs/${type}.stub`, {
-        name: `${name}`,
-        nameUpper,
-        path: this.app.makePath(`app/${type}s/`, `${name}.${type}.ts`),
+        name: `${pluralName}`,
+        nameUpper: pluralNameUpper,
+        path: this.app.makePath(`app/${type}s/`, `${pluralName}.${type}.ts`),
       })
     }
     if (options.includes('v')) {
       const codemods = await this.createCodemods()
       const type = 'validator'
       await codemods.makeUsingStub(stubsRoot, `../commands/stubs/${type}.stub`, {
-        name: `${name}`,
-        nameUpper,
-        path: this.app.makePath(`app/${type}s/`, `${name}.${type}.ts`),
+        name: `${pluralName}`,
+        nameUpper: pluralNameUpper,
+        path: this.app.makePath(`app/${type}s/`, `${pluralName}.${type}.ts`),
       })
     }
     if (options.includes('c')) {
       const codemods = await this.createCodemods()
       const type = 'controller'
       await codemods.makeUsingStub(stubsRoot, `../commands/stubs/${type}.stub`, {
-        name: `${name}`,
-        nameUpper,
-        path: this.app.makePath(`app/${type}s/`, `${name}.${type}.ts`),
+        name: `${pluralName}`,
+        nameUpper: pluralNameUpper,
+        path: this.app.makePath(`app/${type}s/`, `${pluralName}.${type}.ts`),
       })
     }
     if (options.includes('d')) {
       const codemods = await this.createCodemods()
       const type = 'migration'
       await codemods.makeUsingStub(stubsRoot, `../commands/stubs/${type}.stub`, {
-        name: `${name}`,
-        nameUpper,
-        path: this.app.makePath(`database/${type}s/`, `${Date.now()}_create_${name}_table.ts`),
+        name: `${pluralName}`,
+        nameUpper: pluralNameUpper,
+        path: this.app.makePath(
+          `database/${type}s/`,
+          `${Date.now()}_create_${pluralName}_table.ts`
+        ),
       })
     }
   }
